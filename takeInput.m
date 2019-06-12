16645f50d148b848d2c55d394f73fcf04340d623
andrewdoanutz <andrewdoanutz@gmail.com>
2019-06-11 22:55:01 -0700

    sample file

--- takeInput.m
+++ takeInput.m
@@ -5,8 +5,8 @@ function filename=takeInput(option)
        recObj = audiorecorder(44100,16,1);
        recordblocking(recObj, 3);
        myRecording = getaudiodata(recObj);
-       audiowrite('output.wav', myRecording,44100);
-       filename="output.wav";
+       audiowrite('input.wav', myRecording,44100);
+       filename="input.wav";
        fprintf("Done recording\n");
     elseif(option==2)
        filename=input("Enter filename\n",'s');
